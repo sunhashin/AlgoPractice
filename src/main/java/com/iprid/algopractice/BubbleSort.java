c8b021e6749fe09230d49f5646a0ae4f78c1e6c5
iprid <iprid@naver.com>
2022-10-09 12:05:11 +0900

    버블정렬 개선 (버전3)

--- src/main/java/com/iprid/algopractice/BubbleSort.java
+++ src/main/java/com/iprid/algopractice/BubbleSort.java
@@ -11,17 +11,19 @@ public class BubbleSort {
 		a[idx2] = t;
 	}
 	
-	// 버블 정렬 (버전2)
+	// 버블 정렬 (버전3)
 	static void bubbleSort(int[] a, int n) {
-		for (int i = 0; i < n - 1; i++) {
-			int exchg = 0;								// 패스의 교환 횟수를 기록
-			for (int j = n - 1; j > i; j--) {
+		int k = 0;											// a[k] 보다 앞쪽은 정렬을 마친상태
+		while (k < n - 1) {
+			int last = n - 1;								// 마지막으로 요소를 교환한 위치
+			
+			for (int j = n - 1; j > k; j--) {
 				if (a[j - 1] > a[j]) {
 					swap(a, j - 1, j);
-					exchg++;
+					last = j;
 				}
 			}
-			if (exchg == 0) break;				// 교환이 이뤄지지 않으면 종료
+			k = last;
 		}
 	}
 	
