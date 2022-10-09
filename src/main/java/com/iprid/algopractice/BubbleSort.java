011a978ebd8f1eca0d36765b32bcf2536a1c9024
iprid <iprid@naver.com>
2022-10-09 11:48:52 +0900

    bubble sort 개선 (버전2)

--- src/main/java/com/iprid/algopractice/BubbleSort.java
+++ src/main/java/com/iprid/algopractice/BubbleSort.java
@@ -11,14 +11,17 @@ public class BubbleSort {
 		a[idx2] = t;
 	}
 	
-	// 버블 정렬
+	// 버블 정렬 (버전2)
 	static void bubbleSort(int[] a, int n) {
 		for (int i = 0; i < n - 1; i++) {
-			
+			int exchg = 0;								// 패스의 교환 횟수를 기록
 			for (int j = n - 1; j > i; j--) {
-				if (a[j - 1] > a[j])
+				if (a[j - 1] > a[j]) {
 					swap(a, j - 1, j);
+					exchg++;
+				}
 			}
+			if (exchg == 0) break;				// 교환이 이뤄지지 않으면 종료
 		}
 	}
 	
